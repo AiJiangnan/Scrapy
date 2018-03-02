2cd603fbd71b1af1126c2b9531ba313f093b0503
AiJiangnan <aijiangnan@xyb.com>
2018-03-02 10:17:33 +0800

    [update] get Scrapy

--- pythondoc/pipelines.py
+++ pythondoc/pipelines.py
@@ -7,24 +7,17 @@
 import pdfkit
 import re
 import os
+import uuid
 from PyPDF2 import PdfFileMerger
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
-
-csslist = """
-    <link href="css/basic.css" rel="stylesheet" type="text/css">
-    <link href="css/classic.css" rel="stylesheet" type="text/css">
-    <link href="css/pydoctheme.css" rel="stylesheet" type="text/css">
-    <link href="css/pygments.css" rel="stylesheet" type="text/css">
-"""
 
 html_template = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    {css}
 </head>
 <body>
 {content}
@@ -32,10 +25,12 @@ html_template = """
 </html>
 """
 
+css = 'css/sphinx_rtd_theme.css'
+
 
 def parse_url_to_html(body, name):
     html = str(body)
-    html = html_template.format(content=html,css=csslist)
+    html = html_template.format(content=html)
     html = html.encode("utf-8")
     with open(name, 'wb') as f:
         f.write(html)
@@ -45,7 +40,7 @@ def parse_url_to_html(body, name):
 def save_pdf(htmls, file_name):
     options = {'page-size': 'Letter', 'margin-top': '0.75in', 'margin-right': '0.75in', 'margin-bottom': '0.75in', 'margin-left': '0.75in', 'encoding': "UTF-8", 'custom-header': [('Accept-Encoding', 'gzip')],
                'cookie': [('cookie-name1', 'cookie-value1'), ('cookie-name2', 'cookie-value2'), ], 'outline-depth': 10, }
-    pdfkit.from_file(htmls, file_name, options=options)
+    pdfkit.from_file(htmls, file_name, options=options, css=css)
 
 
 class PythondocPipeline(object):
@@ -55,21 +50,25 @@ class PythondocPipeline(object):
         self.pdfs = []
 
     def process_item(self, item, spider):
-        name = item['name'][0]
-        parse_url_to_html(item['data'][0], name + '.html')
-        self.htmls.append(name + '.html')
-        save_pdf(name + '.html', name + '.pdf')
-        self.pdfs.append(name + '.pdf')
+        # name = item['name'][0]
+        # name = str(uuid.uuid1())
+        # parse_url_to_html(item['data'][0], name + '.html')
+        # self.htmls.append(name + '.html')
+        # try:
+        #     save_pdf(name + '.html', name + '.pdf')
+        # except:
+        #     pass
+        # self.pdfs.append(name + '.pdf')
         return item
 
-    def close_spider(self, spider):
-        name = 'Python教程'
-        merger = PdfFileMerger()
-        for pdf in self.pdfs:
-            merger.append(open(pdf, 'rb'))
-        output = open(name+".pdf", "wb")
-        merger.write(output)
-        for html in self.htmls:
-            os.remove(html)
-        for pdf in self.pdfs:
-            os.remove(pdf)
+    # def close_spider(self, spider):
+        # name = 'Scrapy教程'
+        # merger = PdfFileMerger()
+        # for pdf in self.pdfs:
+        #     merger.append(open(pdf, 'rb'))
+        # output = open(name+".pdf", "wb")
+        # merger.write(output)
+        # for html in self.htmls:
+        #     os.remove(html)
+        # for pdf in self.pdfs:
+        #     os.remove(pdf)
--- pythondoc/spiders/picture.py
+++ pythondoc/spiders/picture.py
@@ -1,28 +1,39 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy import Request
+import time
 from pythondoc.items import DocItem
 
 
 class PictureSpider(scrapy.Spider):
     name = 'pythondoc'
-    allowed_domains = ['usyiyi.cn']
-    start_urls = ['http://python.usyiyi.cn/documents/python_352/tutorial/index.html']
+    allowed_domains = ['readthedocs.io']
+    start_urls = ['http://scrapy-chs.readthedocs.io/zh_CN/0.24/index.html']
+    next_urls = []
 
     def parse(self, response):
 
         article_name_xpath = '/html/head/title/text()'
-        article_body_xpath = '//*[@class="section"]'
-        next_url_xpath = '//a[@accesskey="N"]/@href'
-        next_url_prefix = 'http://python.usyiyi.cn/documents/python_352/tutorial/'
+        article_body_xpath = '//div[@itemprop="articleBody"]'
+        next_url_xpath = '//div[@class="wy-menu wy-menu-vertical"]//a//@href'
+        next_url_prefix = 'http://scrapy-chs.readthedocs.io/zh_CN/0.24/'
 
         item = DocItem()
         item['name'] = response.xpath(article_name_xpath).extract()
-        item['data'] = response.xpath(article_body_xpath).extract()
+        # item['data'] = response.xpath(article_body_xpath).extract()
+        item['data'] = response.xpath(next_url_xpath).extract()
         yield item
-        next_url = response.xpath(next_url_xpath).extract()
-        if next_url:
-            next_url = next_url_prefix + next_url[0]
-            yield Request(next_url)
+        next_urls = response.xpath(next_url_xpath).extract()
+        for url in next_urls:
+            next_url = next_url_prefix + url
+            yield Request(next_url, callback=self.parse_subpage)
+
+    def parse_subpage(self, response):
+        article_name_xpath = '/html/head/title/text()'
+        article_body_xpath = '//div[@itemprop="articleBody"]'
+        item = DocItem()
+        item['name'] = response.xpath(article_name_xpath).extract()
+        # item['data'] = response.xpath(article_body_xpath).extract()
+        return item
 
 # scrapy crawl pythondoc
